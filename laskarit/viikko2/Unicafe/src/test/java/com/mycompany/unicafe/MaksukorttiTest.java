55ad2af1332b807c33f0348040b5f2ea100e8547
Petro Lehtonen <petro.lehtonen@gmail.com>
2021-03-25 15:21:03 +0200

    Unicafe testit lisätty

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,46 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
     }
+
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals(1000, kortti.saldo());
+    }
+
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(500);
+        assertEquals(1500, kortti.saldo());
+    }
+
+    @Test
+    public void saldoVaheneeOikeinJosRahaaOnTarpeeksi() {
+        kortti.otaRahaa(500);
+        assertEquals(500, kortti.saldo());
+    }
+
+    @Test
+    public void saldoEiMuutuJosRahaaEiOleTarpeeksi() {
+        kortti.otaRahaa(1500);
+        assertEquals(1000, kortti.saldo());
+    }
+    
+    @Test
+    public void rahanOttaminenPalauttaaOikeanTotuusarvon() {
+        assertEquals(true, kortti.otaRahaa(500));
+        assertEquals(false, kortti.otaRahaa(1500));
+    }
+    
+    @Test
+    public void toStringMetodiPalauttaaOikeanArvon() {
+        assertEquals("saldo: 10.0", kortti.toString());
+    }
+
 }
